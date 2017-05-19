7f7c6c5254eaf02144ec82acbebb422077bf6bca
TimChen <timchen@kkbox.com>
2017-05-19 09:40:20 +0800

    Replace storyboard with xib to be root controller

--- PlayerDemo/AppDelegate.swift
+++ PlayerDemo/AppDelegate.swift
@@ -15,10 +15,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        test1()
         return true
     }
 
+    func test2() {
+        print("Void function")
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window!.backgroundColor = UIColor.white;
+        
+        // Create a nav/vc pair using the custom ViewController class
+        
+        let nav = UINavigationController()
+        
+        
+        // Push the vc onto the nav
+        nav.pushViewController(RootViewController(nibName: "RootViewController", bundle: nil), animated: true)
+        
+        // Set the window’s root view controller
+        //self.window!.rootViewController = nav
+        
+        // Present the window
+        self.window!.makeKeyAndVisible()
+    }
+    
+    func test1() {
+        print("Void function")
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame)
+        let rootView = RootViewController(nibName: "RootViewController", bundle: nil)
+        
+        window?.rootViewController = rootView
+        window?.makeKeyAndVisible()
+        self.window!.backgroundColor = UIColor.white;
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
