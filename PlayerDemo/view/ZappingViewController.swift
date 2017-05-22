1d07b6dd047b2f268cf57cbf68ed48993840a9e9
TimChen <timchen@kkbox.com>
2017-05-22 11:42:37 +0800

    Give scrollView attributes

--- PlayerDemo/view/ZappingViewController.swift
+++ PlayerDemo/view/ZappingViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ZappingViewController: UIViewController , UINavigationControllerDelegate {
+class ZappingViewController: UIViewController , UINavigationControllerDelegate , UIScrollViewDelegate {
+    let COUNT_PAGE = 6
+    var viewControllers: NSMutableArray = []
 
+    @IBOutlet weak var scrollView: UIScrollView!
     // MARK: - UINavigationControllerDelegate method
     public func navigationControllerSupportedInterfaceOrientations(_ navigationController: UINavigationController) -> UIInterfaceOrientationMask {
         return [UIInterfaceOrientationMask.landscapeLeft , UIInterfaceOrientationMask.landscapeRight]
@@ -25,11 +28,40 @@ class ZappingViewController: UIViewController , UINavigationControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setOrientation()
     }
     
     private func shouldAutorotate() -> Bool {
         return true
+    }
+    
+    override func viewDidAppear(_ animated : Bool) {
+        super.viewDidAppear(animated)
+        viewDidLoadForPager()
+    }
+    
+    private func viewDidLoadForPager() {
+        
+        // view controllers are created lazily
+        // in the meantime, load the array with placeholders which will be replaced on demand
+        let controllers: NSMutableArray = [];
+        for index in 1...COUNT_PAGE {
+            print("\(index) times 5 is \(index * 5)")
+            controllers.add("")
+        }
+        self.viewControllers = controllers;
+        
+        scrollView.isPagingEnabled = true
+        
+        //http://ztpala.com/2011/06/22/customize-page-size-uiscrollview/
+        scrollView.clipsToBounds = false
+        scrollView.bounces = false
+
+        let width = scrollView.frame.width * CGFloat(COUNT_PAGE)
+        scrollView.contentSize = CGSize(width: width, height: self.scrollView.frame.height);
+        scrollView.showsHorizontalScrollIndicator = false;
+        scrollView.showsVerticalScrollIndicator = false;
+        scrollView.scrollsToTop = false;
+        scrollView.delegate = self;
     }
 }
