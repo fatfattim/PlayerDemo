eaa25adbdf9f76e7ca8a2925316f6b3d38dc4f7c
TimChen <timchen@kkbox.com>
2017-06-01 11:55:20 +0800

    Support UI test

---
+++ PlayerDemoUITests/PlayerDemoUITests.swift
@@ -0,0 +1,45 @@
+//
+//  PlayerDemoUITests.swift
+//  PlayerDemoUITests
+//
+//  Created by TimChen on 2017/6/1.
+//  Copyright © 2017年 dogtim. All rights reserved.
+//
+
+import XCTest
+
+class PlayerDemoUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCUIDevice.shared().orientation = .faceUp
+        
+        let app = XCUIApplication()
+        
+        app.buttons[NSLocalizedString("Enter Player Page", bundle: Bundle(for: PlayerDemoUITests.self), comment: "")].tap()
+        
+        app.navigationBars["プレーヤー"].buttons["Back"].tap()
+        sleep(10)
+    }
+    
+}
