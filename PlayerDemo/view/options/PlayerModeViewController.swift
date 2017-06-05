b8874df753185be1e94d69bc0c468bfd8e771e5e
TimChen <timchen@kkbox.com>
2017-06-05 15:43:05 +0800

    Create builder to build vc for player

--- PlayerDemo/view/options/PlayerModeViewController.swift
+++ PlayerDemo/view/options/PlayerModeViewController.swift
@@ -8,6 +8,22 @@
 
 import UIKit
 
+
+class PlayerBuilder {
+    static func build(_ uiMode: String, playerMode: String) -> UIViewController {
+        let vc : UIViewController
+        if(uiMode == "Zapping") {
+            vc = ZappingViewController() //change this to your class name
+            
+            vc.title = NSLocalizedString("Zapping", comment: "Zapping Title")
+        } else {
+            vc = PlayerViewController() //change this to your class name
+            vc.title = NSLocalizedString("Player", comment: "Player Title")
+        }
+        return vc
+    }
+}
+
 class PlayerModeViewController: UITableViewController {
     
     var info = [
@@ -45,6 +61,7 @@ class PlayerModeViewController: UITableViewController {
         
         if(indexPath.section == 2) {
             cell.accessoryType = .none
+            cell.textLabel?.textAlignment = .center
         } else if(indexPath.row == 0) {
             cell.accessoryType = .checkmark
         } else {
@@ -61,7 +78,22 @@ class PlayerModeViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if(indexPath.section == 2) {
-            print("Next")
+            
+            var uiMode = ""
+            
+            let rowsCount = self.tableView.numberOfRows(inSection: 0)
+            
+            for i in 0..<rowsCount  {
+                let cell = self.tableView.cellForRow(at: IndexPath(row: i, section: 0))!
+                // your custom code (deselecting)
+                
+                if (cell.accessoryType == .checkmark) {
+                    uiMode = (cell.textLabel?.text)!
+                }
+            }
+            
+            self.navigationController?.pushViewController( PlayerBuilder.build(uiMode, playerMode: ""), animated: true)
+
         } else {
             let selectedCell = tableView.cellForRow(at: indexPath)
             let rowsCount = self.tableView.numberOfRows(inSection: indexPath.section)
@@ -98,52 +130,7 @@ class PlayerModeViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 40
+        return 30
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
 }
